2d59194f8734f7614de89fbaca0c242bad7a24c7
starshine <starshine@imastarshine.ru>
2026-07-31 15:54:43 +0800

    v0.1.7 | Added PROXY_URL for yadisk

--- src/ydisk.py
+++ src/ydisk.py
@@ -1,14 +1,21 @@
 import yadisk
+from yadisk.sessions.async_httpx_session import AsyncHTTPXSession
+
 import src.shared
 import io
+
+proxy_session = AsyncHTTPXSession(proxy=src.shared.YADISK_PROXY) if src.shared.YADISK_PROXY else None
 
 
 class YDisk:
     def __init__(self):
-        self.client: yadisk.AsyncClient = None
+        self.client: yadisk.AsyncClient | None = None
 
     async def init(self):
-        self.client = yadisk.AsyncClient(token=src.shared.YADISK_TOKEN)
+        self.client = yadisk.AsyncClient(
+            token=src.shared.YADISK_TOKEN,
+            session=proxy_session
+        )
 
     async def check(self):
         return await self.client.check_token()
